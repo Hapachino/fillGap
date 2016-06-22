912e3fa44e53f63f58ee1fff7ade363833468f34
cafeKaze <erickwbrownfield@gmail.com>
2016-06-22 00:19:06 -0700

    Update fillGap.py

--- fillGap.py
+++ fillGap.py
@@ -6,43 +6,45 @@ import os, re, shutil
 # Arguments: folder, prefix
 def fillGap(folder, prefix):
 
-	# Create regex with prefix + number + extension     
-	prefixRegex = re.compile(r'(%s)(\d)+(\.[a-zA-Z0-9]+)' % prefix)
+    # Create regex with prefix + number + extension     
+    prefixRegex = re.compile(r'(%s)((\d)+)(\.[a-zA-Z0-9]+)' % prefix)
 
-  	# Make sure folder path is absolute
-  	folder = os.path.abspath(folder)
+    # Make sure folder path is absolute
+    folder = os.path.abspath(folder)
 
-  	# Create list with all files that match regex
-	fileList = []
-  	for item in os.listdir(folder):
-  		if os.path.isfile(os.path.join(folder, item)) and prefixRegex.search(item):
-      			fileList.append(item)
+    # Create list with all files that match regex
+    fileList = []
+    for item in os.listdir(folder):
+        if os.path.isfile(os.path.join(folder, item)) and prefixRegex.search(item):
+            fileList.append(item)
 
-  	# Look for the starting number by incrementing, starting from 0
-	Number = 0  
-	while True:
-  		for filename in fileList:
-		mo = prefixRegex.search(filename)
-			if Number == int(mo.group(2)):
-				fileList.remove(filename)
-				Completed = True
-		  		break
-		Number += 1
-		if Completed == True:
-  			break
+    # Look for the starting number by incrementing, starting from 0
+    Number = 0
+    Completed = False
+    while True:
+        for filename in fileList:
+            mo = prefixRegex.search(filename)
+            if Number == int(mo.group(2)):
+                length = len(str(mo.group(2)))
+                fileList.remove(filename)
+                Completed = True
+                break
+        Number += 1
+        if Completed == True:
+            break
 
-  	# Increment from starting number, finding next largest file and renaming
-	nextNumber = Number
-  	while True:
-  		for filename in fileList:
-			mo = prefixRegex.search(filename)
-			if nextNumber == int(mo.group(2)):
-				# New number including prefixed zero's
-				subNumber = '0' * (len(mo.group(2)) - len(Number)) + str(number)
-				newFileName = prefixRegex.sub(r'\1%s\3' % subNumber, filename)
-				shutil.move(os.path.join(folder, filename), os.path.join(folder, newFileName))
-				fileList.remove(filename)
-				Number += 1
-		if len(fileList) == 0:
-			break	
-		nextNumber += 1
+    # Increment from starting number, finding next largest file and renaming
+    nextNumber = Number
+    while True:
+        for filename in fileList:
+            mo = prefixRegex.search(filename)
+            if nextNumber == int(mo.group(2)):
+                # New number including prefixed zero's
+                subNumber = '0' * (length - len(str((Number)))) + str(Number)
+                newFileName = prefixRegex.sub(r'%s%s\4' % (prefix, subNumber), filename)
+                shutil.move(os.path.join(folder, filename), os.path.join(folder, newFileName))
+                fileList.remove(filename)
+                Number += 1
+        if len(fileList) == 0:
+            break	
+        nextNumber += 1
